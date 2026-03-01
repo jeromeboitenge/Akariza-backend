c2ac85f3bd1ab648ec5111b252c11b3faaf64b3e
boitenge <jeromeboitenge@gmail.com>
2026-03-01 22:36:50 +0200

    Fix: Import EmailModule in SalesModule

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -3,8 +3,10 @@ import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
 import { StockService } from '../stock/stock.service';
 import { NotificationsService } from '../notifications/notifications.service';
+import { EmailModule } from '../email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [SalesController],
   providers: [SalesService, StockService, NotificationsService],
 })
