26da38d596c6752c7c42d030c29485fc819a874b
boitenge <jeromeboitenge@gmail.com>
2026-03-27 17:05:56 +0200

    r

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { CostManagementService } from './cost-management.service';
+import { AuditLoggerService } from '../common/audit-logger.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService, CostManagementService],
+  providers: [ProductsService, CostManagementService, AuditLoggerService],
   exports: [ProductsService, CostManagementService],
 })
 export class ProductsModule {}
